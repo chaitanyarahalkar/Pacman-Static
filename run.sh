e3e1d926ca87abdefab7757551be3f8a7dc3ddf8
chaitanyarahalkar <chaitanyarahalkar4@gmail.com>
2019-12-17 08:20:02 +0530

    Update build file

--- run.sh
+++ run.sh
@@ -6,5 +6,12 @@ mkdir -p /etc/pacman.d/mirrorlist
 cp mirrorlist /etc/pacman.d/mirrorlist
 
 mkdir /var/lib/pacman
+mkdir /usr/share/pacman 
+
+rm -R /etc/pacman.d/gnupg/
+rm -R /root/.gnupg/  # only if the directory exists
+gpg --refresh-keys
+pacman-key --init && pacman-key --populate
+pacman-key --refresh-keys
 
 sudo mv pacman-static /usr/bin/pacman
